fd731f07411639eda33c7dadd393b42077c8449c
John Robin <nannajohn05@gmail.com>
2022-03-19 20:08:36 -0500

    made singleplayer and multiplayer pages

--- hangman.py
+++ hangman.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/singleplayer/")
+def singleplayer():
+    return render_template('singleplayer.html')
+
+@app.route("/multiplayer/")
+def multiplayer():
+    return render_template('multiplayer.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
