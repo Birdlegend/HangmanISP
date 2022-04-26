83228ac7c4c0f29cd69ed871dcae22fa55135cad
¨Griffin <griffin.clark@student.allenisd.org>
2022-04-26 17:36:26 -0500

    I implemented the ability to call the API to get a word and its data

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,29 @@
+//JSON
+const getJSON = async url => {
+    const response = await fetch(url);
+    if (!response.ok)
+	throw new Error(response.statusText);
+
+    const data = response.json();
+    return data;
+}
+console.log("fetching...");
+var word = ""
+var id = ""
+var category = ""
+var numLetters = 0
+getJSON("https://www.wordgamedb.com/api/v1/words/random").then(data => {
+    console.log(data);
+    word = data.word;
+    id = data._id;
+    category = data.category;
+    numLetters = data.numLetters;
+    console.log(`look at this word: ${word}`);
+    console.log(`thats quite the id: ${id}`);
+    console.log(`that's really some category: ${category}`);
+    console.log(`I wish I had that many letters: ${numLetters}`);
+})
+
 const wordbank = ["grim", "valor", "pogchamp"];
 
 const word = wordbank[Math.floor(Math.random() * wordbank.length)]
@@ -6,8 +32,8 @@ console.log(word);
 setTimeout(() => {
     var sTable = document.getElementById("wordTable");
     for (i = 0; i < word.length; i++) {
-        var wordCell = sTable.rows[0].insertCell(word[i])
-        wordCell.setAttribute("contenteditable", "true")
-        wordCell.setAttribute("onkeypress", "return (this.innerText.length <= 0)")
+	var wordCell = sTable.rows[0].insertCell(word[i])
+	wordCell.setAttribute("contenteditable", "true")
+	wordCell.setAttribute("onkeypress", "return (this.innerText.length <= 0)")
     }
 }, "1")
